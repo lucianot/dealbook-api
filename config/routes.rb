a1a8ee198e0018b288f9f7b1e3207ff09ea6814a
Luciano Tavares <lucianomt@gmail.com>
2014-08-10 19:50:31 -0300

    Changes version path to '/1' (from '/v1')

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'welcome#index'
 
-  namespace :v1 do
+  namespace :v1, path: '/1' do
     resources :companies, only: :index
   end
 end
--- spec/routing/companies_routing_spec.rb
+++ spec/routing/companies_routing_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'routes for companies', type: :routing do
   it 'routes /companies to the companies controller' do
-    expect(:get => '/v1/companies').to route_to(
+    expect(:get => '/1/companies').to route_to(
       controller: 'v1/companies',
       action: 'index'
     )
